266011b343b0254555c466b48c362676950640de
ronny-souza <ronnymarinho2@gmail.com>
2025-03-24 21:23:25 -0300

    ✨ feat: adicionando funções de checagem de autenticação, logout e recuperação de dados de usuário no serviço do Keycloak

--- src/app/core/services/uboard-keycloak.service.ts
+++ src/app/core/services/uboard-keycloak.service.ts
@@ -8,7 +8,30 @@ import { User } from '../models/user.model';
 export class UboardKeycloakService {
   private readonly keycloak = inject(Keycloak);
 
+  isAuthenticated(): boolean {
+    return this.keycloak?.authenticated || false;
+  }
+
+  logout() {
+    this.keycloak.logout();
+  }
+
   async getSessionUserProfileData(): Promise<KeycloakProfile> {
     return await this.keycloak.loadUserProfile();
+  }
+
+  async getSessionUser(): Promise<User> {
+    const userProfile = await this.getSessionUserProfileData();
+    return {
+      id: userProfile.id || '',
+      username: userProfile.username || '',
+      email: userProfile.email || '',
+      fullName: `${userProfile.firstName || ''} ${userProfile.lastName || ''}`,
+      firstName: userProfile.firstName || '',
+      lastName: userProfile.lastName || '',
+      avatarUrl:
+        `https://avatar.iran.liara.run/username?username=${userProfile.firstName}+${userProfile.lastName}` ||
+        '',
+    };
   }
 }
