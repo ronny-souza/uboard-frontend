848e16d96226e7aa274e2d0eb6a22b3f3a5dd964
ronny-souza <ronnymarinho2@gmail.com>
2025-03-24 21:23:51 -0300

    ✨ feat: adicionando método de criação de sala no serviço rest api de salas

--- src/app/core/services/api/scrum-poker-rest-api.service.ts
+++ src/app/core/services/api/scrum-poker-rest-api.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { from, Observable, switchMap } from 'rxjs';
 import { ScrumPokerVote } from '../../models/scrum-poker-rooms/scrum-poker-vote.model';
 import { ScrumPokerRoomData } from '../../models/scrum-poker-rooms/srcum-poker-room-data.model';
+import { UboardKeycloakService } from '../uboard-keycloak.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,10 @@ import { ScrumPokerRoomData } from '../../models/scrum-poker-rooms/srcum-poker-r
 export class ScrumPokerRestApiService {
   private readonly baseUrl: string = `${environment.uboardApiUrl}/scrum-poker`;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private uboardKeycloakService: UboardKeycloakService
+  ) {}
 
   listRoomVotes(roomId: string): Observable<ScrumPokerVote[]> {
     return this.httpClient.get<ScrumPokerVote[]>(
@@ -22,6 +26,18 @@ export class ScrumPokerRestApiService {
   getScrumPokerRoom(roomId: string): Observable<ScrumPokerRoomData> {
     return this.httpClient.get<ScrumPokerRoomData>(
       `${this.baseUrl}/room/${roomId}`
+    );
+  }
+
+  createScrumPokerRoom(name: string): Observable<ScrumPokerRoomData> {
+    return from(this.uboardKeycloakService.getSessionUser()).pipe(
+      switchMap((user) => {
+        const body = {
+          name,
+          userIdentifier: user.id,
+        };
+        return this.httpClient.post<ScrumPokerRoomData>(`${this.baseUrl}/room`, body);
+      })
     );
   }
 }
