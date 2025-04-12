33bc5b1d159963f5cfbc93b956c55156379ff673
ronny-souza <ronnymarinho2@gmail.com>
2025-04-11 23:15:13 -0300

    ✨ feat: sincronização do usuário do Keycloak na API ao realizar o login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { UserRestApiService } from './core/services/api/user-rest-api.service';
+import { UboardKeycloakService } from './core/services/uboard-keycloak.service';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,6 +8,17 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `<router-outlet />`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'uboard';
+
+  constructor(
+    private uboardKeycloakService: UboardKeycloakService,
+    private userRestApiService: UserRestApiService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.uboardKeycloakService.isAuthenticated()) {
+      this.userRestApiService.syncUser();
+    }
+  }
 }
