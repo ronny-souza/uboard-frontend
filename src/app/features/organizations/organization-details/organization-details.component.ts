56ea2916fd82d8d021b0bb21b6f5ca4201a5ce46
ronny-souza <ronnymarinho2@gmail.com>
2025-05-01 09:47:08 -0300

    💄 feat: componente de detalhamento de organizações

---
+++ src/app/features/organizations/organization-details/organization-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-details',
+  imports: [],
+  templateUrl: './organization-details.component.html',
+  styleUrl: './organization-details.component.scss'
+})
+export class OrganizationDetailsComponent {
+
+}
